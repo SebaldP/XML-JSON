eaea3c08b74d45c45ecb39828869f37fefcb0b51
Sebald <68822652+SebaldP@users.noreply.github.com>
2024-06-26 15:37:01 +0200

    Delete assets directory

--- assets/Contact-CBRxZ1CA.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,a as o,b as c}from"./index-BHqcrUnC.js";const s={},a={class:"header header-10"},n=c("h1",null,"Contact",-1),r=[n];function _(d,h){return t(),o("header",a,r)}const f=e(s,[["render",_]]);export{f as default};
