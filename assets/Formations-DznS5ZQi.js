9ba614ad5b4826b890457567a4637ccf878e042f
Sebald <68822652+SebaldP@users.noreply.github.com>
2024-06-26 17:14:38 +0200

    v2 - navBar link updated

---
+++ assets/Formations-DznS5ZQi.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as r,a as s,d as a,w as n,b as c,e as _}from"./index-rg3jZcOp.js";const d={},i={class:"header header-10"},l=c("h1",null,"Formations",-1);function h(m,u){const e=t("router-link");return r(),s("header",i,[l,a(e,{to:"/services"},{default:n(()=>[_("Retour")]),_:1})])}const p=o(d,[["render",h]]);export{p as default};
