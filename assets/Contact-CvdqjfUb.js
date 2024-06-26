4a589f926f6b8e22bebbe14095c48dfa29963352
Sebald <68822652+SebaldP@users.noreply.github.com>
2024-06-26 16:57:08 +0200

    v2 - createWebHistory base changed

---
+++ assets/Contact-CvdqjfUb.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,a as o,b as c}from"./index-CLJKrhex.js";const s={},a={class:"header header-10"},n=c("h1",null,"Contact",-1),r=[n];function _(d,h){return t(),o("header",a,r)}const f=e(s,[["render",_]]);export{f as default};
