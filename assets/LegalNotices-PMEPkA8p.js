01a38abd10a0830e5f93352aa71a8cae0853401e
Sebald <68822652+SebaldP@users.noreply.github.com>
2024-06-26 16:47:23 +0200

    v2 - redirection updated

---
+++ assets/LegalNotices-PMEPkA8p.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,a as o,b as t}from"./index-DABWt6Dr.js";const a={},c={class:"header header-10"},n=t("h1",null,"Mentions légales",-1),r=[n];function _(d,l){return s(),o("header",c,r)}const i=e(a,[["render",_]]);export{i as default};
